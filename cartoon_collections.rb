07d9764f6948525bc02c459a49394665316dd745
Shawn Ren <doggy-ren@hotmail.com>
2019-02-22 20:12:34 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,5 @@
 def roll_call_dwarves(dwarves)# code an argument here
-  dwaves.each_with_index do|x, index|
+  dwarves.each_with_index do|x, index|
     puts "#{index} x"
   end # Your code here
 end
