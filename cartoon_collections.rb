2aeb8cff1524192166ceaf0ac83e284b41e40c96
Shawn Ren <doggy-ren@hotmail.com>
2019-02-22 20:12:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,7 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwaves.each_with_index do|x, index|
+    puts "#{index} x"
+  end # Your code here
 end
 
 def summon_captain_planet# code an argument here
